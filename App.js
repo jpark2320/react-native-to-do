764e70a85719a792793ec932a6a10686d90dac86
connectshops <connectshopsco@gmail.com>
2018-06-14 16:17:29 -0700

    Finish the UI halfway through

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+
+const { height, width } = Dimensions.get("window")
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>AnJ To Do</Text>
+        <View style={styles.card}>
+          <TextInput styles={styles.input} placeholder={"New To Do"} />
+        </View>
       </View>
     );
   }
@@ -14,8 +20,36 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  title: {
+    color: '#fff',
+    fontSize: 30,
+    marginTop: 70,
+    marginBottom: 30,
+    fontWeight: "600"
+  },
+  card: {
+    backgroundColor: "#fff",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 5
+      }
+    })
+  }
 });
